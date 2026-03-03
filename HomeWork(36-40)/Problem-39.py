7aef0c82a191b848939b614e28446512332cee23
swetachoudhary543210-beep <swetachoudhary543210@gmail.com>
2026-03-03 23:20:57 +0530

    Create Problem-39.py

---
+++ HomeWork(36-40)/Problem-39.py
@@ -0,0 +1,18 @@
+def sort_colors(nums):
+    low, mid, high = 0, 0, len(nums) - 1
+    
+    while mid <= high:
+        if nums[mid] == 0:
+            nums[low], nums[mid] = nums[mid], nums[low]
+            low += 1
+            mid += 1
+        elif nums[mid] == 1:
+            mid += 1
+        else:
+            nums[mid], nums[high] = nums[high], nums[mid]
+            high -= 1
+    
+    return nums
+
+nums = list(map(int, input().split()))
+print(sort_colors(nums))
