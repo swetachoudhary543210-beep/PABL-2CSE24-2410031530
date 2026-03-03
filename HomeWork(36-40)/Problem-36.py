243de5d2d5fdb0e20d6a0933124ee56c0dfabefc
swetachoudhary543210-beep <swetachoudhary543210@gmail.com>
2026-03-03 23:15:39 +0530

    Create Problem-36.py

---
+++ HomeWork(36-40)/Problem-36.py
@@ -0,0 +1,13 @@
+def plus_one(digits):
+    n = len(digits)
+    
+    for i in range(n-1, -1, -1):
+        if digits[i] < 9:
+            digits[i] += 1
+            return digits
+        digits[i] = 0
+    
+    return [1] + digits
+
+digits = list(map(int, input().split()))
+print(plus_one(digits))
