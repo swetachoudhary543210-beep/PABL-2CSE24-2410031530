a0f0c2072fd55e0a062503fb55ed9dc0eed26897
swetachoudhary543210-beep <swetachoudhary543210@gmail.com>
2026-03-03 22:54:25 +0530

    Create Problem-26.py

---
+++ HomeWork(26-30)/Problem-26.py
@@ -0,0 +1,10 @@
+def find_median(arr):
+    arr.sort()
+    n = len(arr)
+    if n % 2 == 1:
+        return arr[n // 2]
+    else:
+        return (arr[n // 2 - 1] + arr[n // 2]) / 2
+
+arr = list(map(int, input().split()))
+print(find_median(arr))
