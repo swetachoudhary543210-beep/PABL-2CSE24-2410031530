288905cb14878879b94356c681a946f6ba4b9d9b
swetachoudhary543210-beep <swetachoudhary543210@gmail.com>
2026-03-03 23:22:16 +0530

    Create Problem-40.py

---
+++ HomeWork(36-40)/Problem-40.py
@@ -0,0 +1,15 @@
+def subsets(nums):
+    result = []
+    
+    def backtrack(start, path):
+        result.append(path[:])
+        for i in range(start, len(nums)):
+            path.append(nums[i])
+            backtrack(i + 1, path)
+            path.pop()
+    
+    backtrack(0, [])
+    return result
+
+nums = list(map(int, input().split()))
+print(subsets(nums))
