91355a7a005ef0e7c7a383f551dcf9c8a148286a
swetachoudhary543210-beep <swetachoudhary543210@gmail.com>
2026-03-03 23:05:56 +0530

    Create Problem-31.py

---
+++ HomeWork(31-35)/Problem-31.py
@@ -0,0 +1,18 @@
+def search_insert(nums, target):
+    left, right = 0, len(nums) - 1
+    
+    while left <= right:
+        mid = (left + right) // 2
+        
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    
+    return left
+
+nums = list(map(int, input().split()))
+target = int(input())
+print(search_insert(nums, target))
